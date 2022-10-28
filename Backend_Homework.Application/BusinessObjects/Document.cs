f4b306a4db3d457bee262842f19257c413beaf96
Martin Štajnc <stajnc@cngroup.dk>
2022-10-28 21:16:05 +0200

    move Document to businessObjects folder

---
+++ Backend_Homework.Application/BusinessObjects/Document.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend_Homework.Application.BusinessObjects
+{
+    public class Document
+    {
+        public string? Title { get; set; }
+        public string? Text { get; set; }
+    }
+}
