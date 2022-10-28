86f13b02f479b22926b48efbbe84a503176b1552
Martin Štajnc <stajnc@cngroup.dk>
2022-10-28 21:42:05 +0200

    add fileSystem options

---
+++ Backend_Homework.DataAccess/Options/FileSystemStorageOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend_Homework.DataAccess.Options
+{
+    public class FileSystemStorageOptions
+    {
+        public const string FileSystemStorage = "FileSystemStorage";
+
+        public string? RootPath { get; set; }
+        public string? SourceFolderName { get; set; }
+        public string? TargetFolderName { get; set; }
+    }
+}
--- Backend_Homework/Program.cs
+++ Backend_Homework/Program.cs
@@ -10,29 +10,33 @@ using System.Reflection;
 using Backend_Homework.Application.Configuration;
 using System.Collections;
 using Microsoft.Extensions.Hosting;
+using Backend_Homework.DataAccess.Options;
 
 namespace Continero.Homework
 {
     public class Program
     {
+        // https://stackoverflow.com/questions/68392429/how-to-run-net-core-console-app-using-generic-host-builder
         public async static Task Main(string[] args)
         {
             var currentEnvironment = EnvironmentHelper.GetCurrentEnvironment();
-
+            var x = Path.Combine(AssemblyDirectory, "appSettings.json");
             var host = Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(configuration =>
                 {
                     configuration.AddJsonFile(Path.Combine(AssemblyDirectory, "appSettings.json"), false, true);
-                    configuration.AddJsonFile(Path.Combine(AssemblyDirectory, $"appSettings.{currentEnvironment:G}.json"), true, true);
+                    //configuration.AddJsonFile(Path.Combine(AssemblyDirectory, $"appSettings.{currentEnvironment:G}.json"), true, true);
                 })
-                .ConfigureServices(services =>
-                    services.AddOptions()
+                .ConfigureServices((builder,services) =>
+                    services
                         .AddLogging(logger => logger.AddConsole())
+                        .Configure<FileSystemStorageOptions>(builder.Configuration.GetSection(FileSystemStorageOptions.FileSystemStorage))
                         .AddTransient<ControlService>())
                 .ConfigureLogging(configuration =>
                 {
                     configuration.AddConsole();
                 })
+                
                 .Build();
 
             var logger = host.Services.GetService<ILoggerFactory>()
