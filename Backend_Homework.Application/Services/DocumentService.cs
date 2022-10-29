5f0b01c6f4a4798811216db42961366f0cf93768
Martin Štajnc <stajnc@cngroup.dk>
2022-10-29 15:26:32 +0200

    refactor documentService to use converterService and storageService

--- Backend_Homework.Application/Services/DocumentService.cs
+++ Backend_Homework.Application/Services/DocumentService.cs
@@ -1,4 +1,5 @@
 ﻿using Backend_Homework.DataAccess;
+using Backend_Homework.DataAccess.Services;
 using Backend_Homework.DataAccess.Storage;
 using Backend_Homework.DataAccess.Storages.Cloud;
 using Backend_Homework.DataAccess.Storages.FileSystem;
@@ -15,31 +16,37 @@ namespace Backend_Homework.Application.Services
     public class DocumentService : IDocumentService
     {
         private readonly ILogger<DocumentService> _logger;
-        private readonly IFileSystemStorage _fileSystemStorage;
-        private readonly ICloudStorage _cloudStorage;
-        private readonly IWebStorage _webStorage;
+        private readonly IStorageService _storageService;
+        private readonly IConverterService _converterService;
 
         public DocumentService(ILogger<DocumentService> logger,
-            IFileSystemStorage fileSystemStorage,
-            ICloudStorage cloudStorage,
-            IWebStorage webStorage)
+            IStorageService storageService,
+            IConverterService converterService)
         {
             _logger = logger;
-            _fileSystemStorage = fileSystemStorage;
-            _cloudStorage = cloudStorage;
-            _webStorage = webStorage;
+            _storageService = storageService;
+            _converterService = converterService;
         }
 
         public async Task<bool> CheckIfFileExists(StorageType storageType, string fileName)
         {
             _logger.LogDebug($"Document service checks if {fileName} in {storageType} exists");
-            return storageType switch
-            {
-                StorageType.Cloud => await _cloudStorage.DocumentExists(fileName),
-                StorageType.FileSystem => await _fileSystemStorage.DocumentExists(fileName),
-                StorageType.Web => await _webStorage.DocumentExists(fileName),
-                _ => throw new NotImplementedException()
-            };
+            return await _storageService.CheckIfFileExists(storageType, fileName);
+            
+        }
+
+        public Task<string> FormatText(string text, FormatType resultFormatType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetDataFromFile<T>(StorageType storageType, string fileName)
+        {
+            _logger.LogDebug($"Document service retrieves {fileName} from {storageType}");
+            var fileContent = await _storageService.GetTextFromFile(storageType, fileName);
+            var fileExtension = Path.GetExtension(fileName);
+
+            return await _converterService.ConvertStringToObject<T>(fileContent, fileExtension);
         }
     }
 }
--- Backend_Homework.Application/Services/IDocumentService.cs
+++ Backend_Homework.Application/Services/IDocumentService.cs
@@ -9,6 +9,6 @@ namespace Backend_Homework.Application.Services
 {
     public interface IDocumentService
     {
-        Task<bool> CheckIfFileExists(StorageType storageType, string fileName);
+        Task<string> FormatText(string text, FormatType resultFormatType);
     }
 }
